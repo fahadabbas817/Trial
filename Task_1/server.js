090447eb56d29cbe3895618a646fb3a6b9c34c55
Syed Fahad Abbas <110738135+fahadabbas817@users.noreply.github.com>
2025-09-02 19:10:30 +0500

    added the default route to server.js

--- Task_1/server.js
+++ Task_1/server.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import express from  'express';
-import helmet from 'helmet';
+
 import mongoose from 'mongoose';
 import cors from 'cors';
 import tasksRoute from './src/routes/tasks.js';
 import errorHandler from './src/middleware/errorHandler.js';
 
 const app = express();
-app.use(helmet());
+
 app.use(express.json());
 app.use(cors());
 
